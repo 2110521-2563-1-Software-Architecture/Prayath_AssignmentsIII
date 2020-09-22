f0289f2e9fa8a9a0c3197f059ada3994e10eee54
N <N@N.N>
2020-09-23 00:17:47 +0700

    mvvm

--- mvvm/mvvm/views/main_view.py
+++ mvvm/mvvm/views/main_view.py
@@ -11,6 +11,7 @@ class MainView(BaseView):
         BaseView.__init__(self, "MVVM Note Application")
         # Create view model
         # Your code here
+        self.vm = MainViewModel()
 
         self.init_ui()
 
@@ -20,7 +21,7 @@ class MainView(BaseView):
     def bind_observable(self):
         # Subscribe to the notes behavior subject and update the view when the data change
         # Your code here
-        pass
+        self.vm.behavior.subscribe(lambda note: self.update_view(note))
 
     def init_ui(self):
         panel = wx.Panel(self)
@@ -57,10 +58,10 @@ class MainView(BaseView):
     def on_clear_all_button_clicked(self, e):
         # Clear all notes
         # Your code here
-        pass
+        self.vm.clear_all()
 
     def on_add_note_button_clicked(self, e):
         content = self.note_input.GetValue()
         self.note_input.SetValue("")
         # Add new note
-        # Your code here
+        self.vm.add_note(content)
